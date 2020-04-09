ddae633148cb2d34aebd83e83f18ef1f008df540
Bhavesh Kalisetti <bhaveshk658@berkeley.edu>
2020-04-08 22:55:05 -0700

    Working on extract.py

--- extract.py
+++ extract.py
@@ -8,7 +8,24 @@ from math import hypot
 import os
 import time
 
-from interact import distance
+from interact_toolset import distance
 
-print(2)
-print(distance(0, 0, 0, 2))
+def get_training_data(k, location):
+	'''
+	Get training data from k files from string location.
+	E.g. get_training_data(4, "DR_USA_Roundabout_EP")
+	Returns a dataframe of k files compiled together.
+	'''
+	frames = []
+
+	for i in range(k):
+		data = pd.read_csv(os.path.join("interaction-dataset-copy/recorded_trackfiles/"
+			+location, "vehicle_tracks_00"+str(k)+".csv"))
+		frames.append(data)
+
+	return pd.concat(frames)
+
+def 
+
+
+
