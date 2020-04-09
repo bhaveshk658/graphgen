33bd0fd2885a02ba03a84469c5c4faff0816643d
Bhavesh Kalisetti <bhaveshk658@berkeley.edu>
2020-04-08 23:11:27 -0700

    Started relevant papers section

--- extract.py
+++ extract.py
@@ -25,7 +25,30 @@ def get_training_data(k, location):
 
 	return pd.concat(frames)
 
-def 
+def plot_all_data(data):
+	'''
+	Plots data returned by get_training_data.
+	Allows us to get visualization of what the map can look like.
+	Isolates x and y coordinates from dataframe and plots.
+	'''
+	temp = data.to_numpy()
+	temp = np.vstack((temp[:, 4], temp[:, 5])).T
+	plt.scatter(temp[:, 0], temp[:, 1], s=1)
+	plt.title("All Data Points")
+	plt.xlabel("X-Coordinate")
+	plt.ylabel("Y-Coordinate")
+	plt.show()
+
+def get_clusters(data):
+	'''
+	Perform k-means clustering on data.
+	Returns array of clusters (coordinates).
+	'''
+	
+
+
+if __name__ == "__main__":
+	data = get_training_data(4, "DR_USA_Roundabout_EP")
 
 
 
