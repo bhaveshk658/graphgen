922a3a83f3898ff90ba7c8bcbad1c665ab2727a5
Bhavesh Kalisetti <bhaveshk658@berkeley.edu>
2020-04-08 22:30:54 -0700

    Started extract.py

---
+++ extract.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from math import hypot
+
+import os
+import time
+
+from interact import distance
+
+print(distance(0, 0, 0, 2))
