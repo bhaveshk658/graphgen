4502362efd03895cd29266760090e1d78a16ea98
bhaveshk658 <bhaveshk658@berkeley.edu>
2021-06-21 23:52:30 -0700

    Raw traces for DR_CHN_Roundabout_LN

--- examples/write_to_file.py
+++ examples/write_to_file.py
@@ -4,28 +4,28 @@ from tqdm import tqdm
 
 from graphgen.data import get_training_data, clean, gravity, compute_headings
 
-path = "/Users/bkalisetti658/desktop/graphgen/data/InteractionDR1.0/recorded_trackfiles/DR_USA_Roundabout_EP"
+path = "/Users/bkalisetti658/desktop/graphgen/data/InteractionDR1.0/recorded_trackfiles/DR_CHN_Roundabout_LN"
 
 # Get data within a certain box
 print("Fetching data...")
-traces = get_training_data([1, 2, 3, 5, 6, 7], path)
+traces = get_training_data([0, 1, 2, 3], path)
 
 print("Saving...")
-np.save(file="/Users/bkalisetti658/desktop/graphgen/data/DR_USA_Roundabout_EP/traces1.npy", arr=traces)
+np.save(file="/Users/bkalisetti658/desktop/graphgen/data/DR_CHN_Roundabout_LN/raw_traces.npy", arr=traces)
 
-print("Cleaning data...")
-traces = clean(traces, length_threshold=50, dist_threshold=2)
+# print("Cleaning data...")
+# traces = clean(traces, length_threshold=50, dist_threshold=2)
 
-print("Saving...")
-np.save(file="/Users/bkalisetti658/desktop/graphgen/data/DR_USA_Roundabout_EP/traces2.npy", arr=traces)
+# print("Saving...")
+# np.save(file="/Users/bkalisetti658/desktop/graphgen/data/DR_USA_Roundabout_EP/traces2.npy", arr=traces)
 
-traces = gravity(traces, resultant_threshold=0.3)
+# traces = gravity(traces, resultant_threshold=0.3)
 
-print("Saving...")
-np.save(file="/Users/bkalisetti658/desktop/graphgen/data/DR_USA_Roundabout_EP/traces3.npy", arr=traces)
+# print("Saving...")
+# np.save(file="/Users/bkalisetti658/desktop/graphgen/data/DR_USA_Roundabout_EP/traces3.npy", arr=traces)
 
-print("Computing headings")
-traces = compute_headings(traces)
+# print("Computing headings")
+# traces = compute_headings(traces)
 
-print("Saving...")
-np.save(file="/Users/bkalisetti658/desktop/graphgen/data/DR_USA_Roundabout_EP/traces.npy", arr=traces)
+# print("Saving...")
+# np.save(file="/Users/bkalisetti658/desktop/graphgen/data/DR_USA_Roundabout_EP/traces.npy", arr=traces)
